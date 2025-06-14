075919406cda3c4bc1e7f631902dc7211f73da68
EruditeBoen <boen.erudition@gmail.com>
2025-06-14 17:06:22 -0400

    Update ProjectsSection.jsx

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -14,7 +14,7 @@ const projects = [
 		id: 1,
 		title: "Georgia Tech",
 		description: "",
-		image: "/public/projects/GT.png",
+		image: "/AkiliUnivWebsite/projects/GT.png",
 		tags: [],
 		demoUrl: "https://www.gatech.edu/",
 	},
